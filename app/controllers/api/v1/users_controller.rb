e208622c282435eedeb3cd6ddae011b75c898762
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-08-18 16:16:15 -0400

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
   def show
     respond_with User.find(params[:id])
   end
-  
+
   def create
     user = User.new(user_params)
     if user.save
@@ -13,6 +13,16 @@ class Api::V1::UsersController < ApplicationController
       render json: { errors: user.errors }, status: 422
     end
   end
+
+	def update
+	  user = User.find(params[:id])
+
+	  if user.update(user_params)
+	    render json: user, status: 200, location: [:api, user]
+	  else
+	    render json: { errors: user.errors }, status: 422
+	  end
+	end
 
   private
 
