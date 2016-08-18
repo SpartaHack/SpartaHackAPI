b7934b6754e8003c5026e5bafcc8dcc0273b415c
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-08-18 15:29:25 -0400

    Adds devise user model

--- Gemfile
+++ Gemfile
@@ -40,5 +40,13 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :test do
+  gem "rspec-rails", "~> 2.14"
+  gem "factory_girl_rails"
+  gem 'ffaker'
+end
+
+gem "devise"
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+  devise_for :users
   # Api definition
   namespace :api, defaults: { format: :json },
-                                constraints: { subdomain: 'api' }, path: '/'  do
-       
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+
+    end
   end
 end
