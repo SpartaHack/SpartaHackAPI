45f944d374015f999d17004af24710c0caf98c04
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-08-18 15:50:02 -0400

    Adds show action the users controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,9 @@ describe User do
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
-end
 
-describe "when email is not present" do
-  before { @user.email = " " }
-  it { should_not be_valid }
+	it { should validate_presence_of(:email) }
+	it { should validate_uniqueness_of(:email) }
+	it { should validate_confirmation_of(:password) }
+	it { should allow_value('example@domain.com').for(:email) }
 end
