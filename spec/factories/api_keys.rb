6e9413d253fe176e43a80f7d17f2c06c2a0e3678
Joshua Miles <milesjos@msu.edu>
2016-08-29 13:28:02 -0400

    :pencil: Add annotate gem and annotate routes and models

--- spec/factories/api_keys.rb
+++ spec/factories/api_keys.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: api_keys
+#
+#  id           :integer          not null, primary key
+#  access_token :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 FactoryGirl.define do
   factory :api_key do
     access_token "auniquetoken123"
