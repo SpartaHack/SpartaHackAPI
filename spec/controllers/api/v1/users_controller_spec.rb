a4ee9a68505e51bc58b531a76a3b92c8a42dce11
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-08-31 02:45:42 -0400

    :pencil: Updates tests for remote connectivity issue fix

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -39,7 +39,7 @@ describe Api::V1::UsersController do
     context "when is successfully created" do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for :user
-        post :create, { user: @user_attributes }
+        post :create, @user_attributes
       end
 
       it "renders the json representation for the user record just created" do
@@ -53,7 +53,7 @@ describe Api::V1::UsersController do
     context "when is not created" do
       before(:each) do
         @invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" } #notice I'm not including the email
-        post :create, { user: @invalid_user_attributes }
+        post :create, @invalid_user_attributes
       end
 
       it "renders an errors json" do
@@ -61,7 +61,7 @@ describe Api::V1::UsersController do
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json errors on whye the user could not be created" do
+      it "renders the json errors on why the user could not be created" do
         user_response = json_response
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
@@ -78,7 +78,7 @@ describe Api::V1::UsersController do
 
     context "when is successfully updated" do
       before(:each) do
-        patch :update, { id: @user.id, user: { email: "newmail@example.com" } }
+        patch :update, { id: @user.id, email: "newmail@example.com" }
       end
 
       it "renders the json representation for the updated user" do
@@ -91,7 +91,7 @@ describe Api::V1::UsersController do
 
     context "when is not updated" do
       before(:each) do
-        patch :update, { id: @user.id, user: { email: "bademail.com" } }
+        patch :update, { id: @user.id, email: "bademail.com" }
       end
 
       it "renders an errors json" do
