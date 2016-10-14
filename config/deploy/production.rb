260abee9538ef638bf488334b8c714f13751580a
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-10-14 05:47:50 +0000

    smh

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '159.203.124.166', port: 22, roles: [:web, :app, :db], primary: true
+server '104.236.30.243', port: 22, roles: [:web, :app, :db], primary: true
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
