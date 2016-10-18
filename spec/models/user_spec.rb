328716ae3d888976883694cdbeb02dc490f5f313
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-10-18 18:29:51 -0400

    :pencil: Adds tests for new user columns

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,6 +37,8 @@ RSpec.describe User do
   subject { @user }
 
   it { should respond_to(:email) }
+  it { should respond_to(:first_name) }
+  it { should respond_to(:last_name) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:auth_token) }
@@ -45,6 +47,8 @@ RSpec.describe User do
 
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
   it { should validate_uniqueness_of(:auth_token)}
--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -9,18 +9,27 @@ RSpec.describe UserSerializer, :type => :serializer do
     end
 
     it "should only include id, email, auth_token, and roles as an attribute" do
-      expect(@serialized.keys).to eq ["id", "email", "auth_token", "roles"]
+      expect(@serialized.keys).to eq ["id", "email", "first_name", "last_name",
+        "auth_token", "roles"]
     end
 
     it "should match id as an attribute" do
       expect(@serialized["id"]).to eq @director.id
     end
 
-    it "should match question as an attribute" do
+    it "should match email as an attribute" do
       expect(@serialized["email"]).to eq @director.email
     end
 
-    it "should match answer as an attribute" do
+    it "should match first name as an attribute" do
+      expect(@serialized["first_name"]).to eq @director.first_name
+    end
+
+    it "should match last name as an attribute" do
+      expect(@serialized["last_name"]).to eq @director.last_name
+    end
+
+    it "should match auth token as an attribute" do
       expect(@serialized["auth_token"]).to eq @director.auth_token
     end
 
