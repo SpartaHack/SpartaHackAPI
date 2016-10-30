0bb2b2adb52465c31fbbba0bf4d1400f8e73e595
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-10-30 12:27:45 -0400

    :beetle: Fixes web bug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -39,7 +39,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation,
-        :first_name, :last_name, roles: [])
+      params.permit(:email, :password, :password_confirmation,
+        :first_name, :last_name, roles: []).merge(:roles => ['hacker'])
     end
 end
