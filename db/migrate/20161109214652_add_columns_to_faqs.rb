4909279dab45babdef4938136b1ae7a049a8aa7d
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-11-10 00:00:16 -0500

    :beetle: Fix existing tables

--- db/migrate/20161109214652_add_columns_to_faqs.rb
+++ db/migrate/20161109214652_add_columns_to_faqs.rb
@@ -1,6 +1,8 @@
 class AddColumnsToFaqs < ActiveRecord::Migration[5.0]
   def change
     add_column :faqs, :display, :boolean, :null => false
+    change_column_null :faqs, :display, false
     add_column :faqs, :priority, :integer, :null => false
+    change_column_null :faqs, :priority, false
   end
 end
