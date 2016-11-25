05664234ea6fac5563c1f5c0ae2aa47dc15414e1
Noah <hinesnoa@msu.edu>
2016-11-25 16:11:05 -0600

    Updated the dummy announcements
    Let me know if this looks okay, Bogdan.

--- app/controllers/api/v1/announcements_controller.rb
+++ app/controllers/api/v1/announcements_controller.rb
@@ -3,9 +3,11 @@ class Api::V1::AnnouncementsController < ApplicationController
   # GET /announcements
   def index
     render json: { announcements: [
-      { id: 1, title: "explain to you how all this", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco", pinned: 0, createdAt: "2016-09-29T02:58:43+00:00", updatedAt: "2016-09-29T05:48:43+00:00"},
-      { id: 2, title: "At vero eos et accusamus et iusto odio", description:"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system", pinned: 0, createdAt: "2016-09-29T04:48:43+00:00", updatedAt: "2016-09-29T05:58:43+00:00"},
-      { id: 3, title: "quo minus id", description:"At vero eos et accusamus et iusto odio", pinned: 1, createdAt: "2016-09-29T06:48:43+00:00", updatedAt: "2016-09-29T06:48:43+00:00"}
+      { id: 1, title: "SpartaHack", description:"Check out what we have planned for you at https://spartahack.com", pinned: 1, createdAt: "2016-09-29T02:58:43+00:00", updatedAt: "2016-09-29T05:48:43+00:00"},
+      { id: 2, title: "About SpartaHack", description:"On the weekend of January 20th, we're bringing together 500 students of all skill levels and disciplines to get creative with tech, connect with peers and professionals, and forget the words \"I can't.\"", pinned: 0, createdAt: "2016-09-29T04:48:43+00:00", updatedAt: "2016-09-29T05:58:43+00:00"},
+      { id: 3, title: "Go, go, go!", description:"It's 11:00pm! Let the hacking begin!", pinned: 0, createdAt: "2016-09-29T06:48:43+00:00", updatedAt: "2016-09-29T06:48:43+00:00"},
+      { id: 4, title: "Announcement Title", description:"This is Noah's fake Announcement. He is picky and wanted more dummy Announcements.", pinned: 0, createdAt: "2016-09-29T06:48:43+00:00", updatedAt: "2016-09-29T06:48:43+00:00"},
+      { id: 5, title: "Announcement Title", description:"This is a short Announcement.", pinned: 0, createdAt: "2016-09-29T06:48:43+00:00", updatedAt: "2016-09-29T06:48:43+00:00"}
     ] }, status: 200
   end
 
