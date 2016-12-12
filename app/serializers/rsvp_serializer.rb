53a7519e9125738f235ebe83319248e23f8146c8
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-12-11 21:27:04 -0500

    :racehorse: Performance boost

--- app/serializers/rsvp_serializer.rb
+++ app/serializers/rsvp_serializer.rb
@@ -1,6 +1,6 @@
 class RsvpSerializer < ActiveModel::Serializer
   attributes :id, :user, :attending, :dietary_restrictions,
-  :other_dietary_restrictions, :shirt_size, :carpool_sharing, :jobs, :resume
+  :other_dietary_restrictions, :shirt_size, :carpool_sharing, :jobs
 
   belongs_to :user
 
